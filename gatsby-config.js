a518f58b84ab22abc75b86e2bae690f542b3aa2c
Pablo Albaladejo <pablo.albaladejo@guidesmiths.com>
2021-02-11 23:36:15 +0100

    chore(master): add Google Tag Manager

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,14 @@
-module.exports = {
-    plugins: [
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`
+})
 
-    ]
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        trackingIds: [process.env.GA_TRACKING_ID],
+      },
+    },
+  ],
 }
