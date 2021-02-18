bae634e812dff22065ae41a3394a21a7d92258ac
Pablo Albaladejo <pablo.albaladejo@guidesmiths.com>
2021-02-18 12:10:37 +0100

    feat(master): add profile initial styling

--- src/components/containers/profile/index.js
+++ src/components/containers/profile/index.js
@@ -1,12 +1,29 @@
 import * as React from "react"
 
+/** @jsx jsx */
+import { jsx } from "theme-ui"
+
 import avatarImg from '../../../images/avatar.jpeg'
+import backgroundImg from '../../../images/background.jpg'
 import Avatar from "../../commons/avatar"
 import Social from "../social"
 
 const Profile = () => {
   return (
-    <div>
+    <div sx={{
+      backgroundAttachment: 'scroll',
+      backgroundPosition: '0 0, 50%',
+      backgroundSize: 'auto, cover',
+      backgroundColor: "primary",
+      backgroundImage: `url(${backgroundImg})`,
+      left: 'auto',
+      padding: ' 6em 4em',
+      position: 'relative',
+      textAlign: 'center',
+      top: 'auto',
+      display: 'block',
+      width: ['100%', '100%', '30%']
+    }}>
       <Avatar img={avatarImg} />
       <h1>
         <strong>I'm Pablo Albaladejo</strong>, a software engineer expert in challenges
